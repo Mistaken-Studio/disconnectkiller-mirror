64153512d4fc99bd295facf6af566f0cab6db250
Xname <xnameserver@o2.pl>
2022-10-22 18:13:40 +0200

    C# 9 and make vaporize players when they leave the server

--- DisconnectKiller/KillPlayerHandler.cs
+++ DisconnectKiller/KillPlayerHandler.cs
@@ -4,21 +4,19 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-using System;
 using System.Linq;
-using System.Reflection;
 using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Roles;
 using Mistaken.API;
-using Mistaken.API.Diagnostics;
 using Mistaken.API.Extensions;
 using Mistaken.RoundLogger;
+using PlayerStatsSystem;
 using UnityEngine;
 
 namespace Mistaken.DisconnectKiller
 {
-    internal class KillPlayerHandler : API.Diagnostics.Module
+    internal sealed class KillPlayerHandler : API.Diagnostics.Module
     {
         public KillPlayerHandler(PluginHandler p)
             : base(p)
@@ -26,7 +24,7 @@ namespace Mistaken.DisconnectKiller
             Instance = this;
         }
 
-        public override string Name => "KillPlayer";
+        public override string Name => nameof(KillPlayerHandler);
 
         public override void OnEnable()
         {
@@ -40,12 +38,9 @@ namespace Mistaken.DisconnectKiller
             Exiled.Events.Handlers.Scp106.Containing -= this.Scp106_Containing;
         }
 
-        internal static KillPlayerHandler Instance { get; private set; }
-
-        internal void RespawnSCP(Player player)
+        public void RespawnSCP(Player player)
         {
             RLogger.Log("SCP RESPAWN", "SCP", $"Respawning SCP, Current: {player.PlayerToString()}");
-
             var spectators = RealPlayers.Get(Team.RIP).Where(x => !x.IsOverwatchEnabled).ToArray();
 
             if (spectators.Length == 0)
@@ -53,49 +48,48 @@ namespace Mistaken.DisconnectKiller
                 player.IsGodModeEnabled = false;
                 MapPlus.Broadcast("RESPAWN", 10, $"SCP player Change, ({player.Id}) {player.Nickname} -> Nobody", Broadcast.BroadcastFlags.AdminChat);
                 player.Kill("Unknown cause of death", "TERMINATED SUCCESSFULLY");
+                return;
             }
-            else
+
+            var randomPlayer = spectators[UnityEngine.Random.Range(0, spectators.Length)];
+
+            var position = player.Position + (Vector3.up * 0.5f);
+            var hp = player.Health;
+            var ahp = player.ArtificialHealth;
+            player.Role.Is<Scp079Role>(out var scp079role);
+            randomPlayer.SetRole(player.Role.Type, SpawnReason.ForceClass, false);
+            void LateSync()
             {
-                var randomPlayer = spectators[UnityEngine.Random.Range(0, spectators.Length)];
-
-                var position = player.Position + (Vector3.up * 0.5f);
-                var hp = player.Health;
-                var ahp = player.ArtificialHealth;
-                Scp079Role scp079role = null;
-                player.Role.Is<Scp079Role>(out scp079role);
-                randomPlayer.SetRole(player.Role, SpawnReason.ForceClass, false);
-                this.CallDelayed(
-                    .5f,
-                    () =>
-                    {
-                        if (scp079role is null)
-                        {
-                            randomPlayer.Health = hp;
-                            randomPlayer.ArtificialHealth = ahp;
-                        }
-                        else
-                        {
-                            var rscp = randomPlayer.Role.As<Scp079Role>();
-                            rscp.Level = scp079role.Level;
-                            rscp.MaxEnergy = scp079role.MaxEnergy;
-                            rscp.Energy = scp079role.Energy;
-                            rscp.Experience = scp079role.Experience;
-                            if (!(scp079role.Camera is null))
-                                rscp.SetCamera(scp079role.Camera);
-                        }
-                    },
-                    "KillPlayerHandler.LateSync");
-
                 if (scp079role is null)
-                    this.CallDelayed(.5f, () => randomPlayer.Position = position, "KillPlayerHandler.LateTeleport");
-
-                player.SetRole(RoleType.Spectator, SpawnReason.None);
-                randomPlayer.Broadcast(10, $"Player {player.GetDisplayName()} left game so you were moved to replace him");
-                MapPlus.Broadcast("RESPAWN", 10, $"SCP player Change, ({player.Id}) {player.Nickname} -> ({randomPlayer.Id}) {randomPlayer.Nickname}", Broadcast.BroadcastFlags.AdminChat);
+                {
+                    randomPlayer.Health = hp;
+                    randomPlayer.ArtificialHealth = ahp;
+                }
+                else
+                {
+                    var rscp = randomPlayer.Role.As<Scp079Role>();
+                    rscp.Level = scp079role.Level;
+                    rscp.MaxEnergy = scp079role.MaxEnergy;
+                    rscp.Energy = scp079role.Energy;
+                    rscp.Experience = scp079role.Experience;
+                    if (scp079role.Camera is not null)
+                        rscp.SetCamera(scp079role.Camera);
+                }
             }
+
+            this.CallDelayed(0.5f, LateSync, nameof(LateSync));
+
+            if (scp079role is null)
+                this.CallDelayed(.5f, () => randomPlayer.Position = position, "KillPlayerHandler.LateTeleport");
+
+            player.SetRole(RoleType.Spectator, SpawnReason.None);
+            randomPlayer.Broadcast(10, $"Player {player.GetDisplayName()} left game so you were moved to replace him");
+            MapPlus.Broadcast("RESPAWN", 10, $"SCP player Change, ({player.Id}) {player.Nickname} -> ({randomPlayer.Id}) {randomPlayer.Nickname}", Broadcast.BroadcastFlags.AdminChat);
         }
 
-        private bool scp106contained = false;
+        internal static KillPlayerHandler Instance { get; private set; }
+
+        private static bool _scp106contained = false;
 
         private void RespawnPlayer(Player currentPlayer)
         {
@@ -105,17 +99,17 @@ namespace Mistaken.DisconnectKiller
             if (currentPlayer.IsDead)
                 return;
 
-            if (currentPlayer.Role == RoleType.Scp106 && this.scp106contained)
+            if (currentPlayer.Role.Type == RoleType.Scp106 && _scp106contained)
                 return;
 
-            if (currentPlayer.IsScp && currentPlayer.Role != RoleType.Scp0492)
+            if (currentPlayer.IsScp && currentPlayer.Role.Type != RoleType.Scp0492)
             {
                 this.RespawnSCP(currentPlayer);
             }
             else
             {
-                currentPlayer.Kill("Heart Attack");
                 RLogger.Log("DISCONNECT KILLER", "HUMAN", $"Killing human {currentPlayer.PlayerToString()}");
+                currentPlayer.ReferenceHub.playerStats.KillPlayer(new DisruptorDamageHandler(new Footprinting.Footprint(Server.Host.ReferenceHub), -1f));
             }
         }
 
@@ -127,7 +121,7 @@ namespace Mistaken.DisconnectKiller
 
         private void Scp106_Containing(Exiled.Events.EventArgs.ContainingEventArgs ev)
         {
-            this.scp106contained = true;
+            _scp106contained = true;
         }
     }
 }
--- DisconnectKiller/PluginHandler.cs
+++ DisconnectKiller/PluginHandler.cs
@@ -7,28 +7,28 @@
 using System;
 using Exiled.API.Enums;
 using Exiled.API.Features;
+using Mistaken.Updater.API.Config;
 
 namespace Mistaken.DisconnectKiller
 {
-    /// <inheritdoc/>
-    public class PluginHandler : Plugin<Config>
+    internal sealed class PluginHandler : Plugin<Config>, IAutoUpdateablePlugin
     {
-        /// <inheritdoc/>
         public override string Author => "Mistaken Devs";
 
-        /// <inheritdoc/>
         public override string Name => "DisconnectKiller";
 
-        /// <inheritdoc/>
         public override string Prefix => "MDisconnectKiller";
 
-        /// <inheritdoc/>
         public override PluginPriority Priority => PluginPriority.Low;
 
-        /// <inheritdoc/>
-        public override Version RequiredExiledVersion => new Version(5, 1, 3);
+        public override Version RequiredExiledVersion => new(5, 2, 2);
 
-        /// <inheritdoc/>
+        public AutoUpdateConfig AutoUpdateConfig => new()
+        {
+            Type = SourceType.GITLAB,
+            Url = "https://git.mistaken.pl/api/v4/projects/70",
+        };
+
         public override void OnEnabled()
         {
             Instance = this;
@@ -40,7 +40,6 @@ namespace Mistaken.DisconnectKiller
             base.OnEnabled();
         }
 
-        /// <inheritdoc/>
         public override void OnDisabled()
         {
             API.Diagnostics.Module.OnDisable(this);
